d1ff5a3f2f5fc22b1cf7c4e3d8a4061ef1d2c77b
dayvid-dias-sp-tech <dayvid.sdias@sptech.school>
2026-09-10 10:29:50 -0300

    feat: Adiciona pasta Arduino com script do sensor LM35

---
+++ Assets/Arduino/sketch_sensor.ino
@@ -0,0 +1,25 @@
+const int pinoSensorLM35 = A0;
+float temperatura;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+  void loop() {
+  int valorLido = analogRead(pinoSensorLM35);
+  temperatura = (valorLido * 0.00488);
+  temperatura = temperatura * 100;
+
+  // para declarar labels no plotter serial:
+  Serial.print("TempMaximo:");
+  Serial.print(4);
+  Serial.print(" ");
+  Serial.print("Temperatura:");
+  Serial.print(temperatura - 23);
+  Serial.print(" ");
+  Serial.print("TempMinimo:");
+  Serial.println(0);
+
+  delay(2000);
+}
+
